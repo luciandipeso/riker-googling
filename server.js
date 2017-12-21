5ac6d7552d06defbc8e23c5c1b82c288604c5361
Lucian DiPeso <ldipeso@gmail.com>
2017-12-21 00:05:05 -0800

    Updated riker googling to latest packages/switched to better-sqlite3.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var bunyan = require('bunyan');
-var express = require('express');
-var serveStatic = require('serve-static');
-var compress = require('compression');
-var sqlite = require('sqlite3').verbose();
-var validator = require('validator');
-var fs = require('fs');
-var path = require('path');
-var twitter = require('twitter');
-var bigInt = require('big-integer');
-var nconf = require('nconf');
+const bunyan = require('bunyan')
+const compression = require('compression')
+const express = require('express')
+const serveStatic = require('serve-static')
+const Database = require('better-sqlite3')
+const validator = require('validator')
+const fs = require('fs')
+const path = require('path')
+const twitter = require('twitter')
+const bigInt = require('big-integer')
+const nconf = require('nconf')
 
 nconf.argv()
-     .env();
+     .env()
 
-var env = nconf.get('NODE_ENV') || "dev";
-nconf.file({ file: __dirname + "/conf/" + env + ".conf.json" });
+const env = ["prod", "dev"].includes(nconf.get('NODE_ENV')) ? nconf.get('NODE_ENV') : "dev"
+nconf.file({ file: path.join(__dirname, "conf", env + ".conf.json") })
 
 /**
  * Logging
@@ -33,20 +33,20 @@ var log = bunyan.createLogger({
       path: __dirname + "/logs/app.log"
     }
   ]
-});
+})
 
 /**
  * Cache of Riker tweets
  */
-var dbFile = 'riker.db',
-    db = new sqlite.Database(dbFile, sqlite.OPEN_READONLY);
+const dbFile = path.join(__dirname, nconf.get('dbPath'), nconf.get('dbFile')) || "riker.db"
+const db = new Database(dbFile, { readonly: true })
 
 /**
  * UI Server
  */
-var server = express();
-server.use(compress());
-server.listen(nconf.get("serverPort"));
+var server = express()
+server.use(compression())
+server.listen(nconf.get("serverPort") || 3000)
 
 /**
  * Twitter client
@@ -56,7 +56,7 @@ var client = new twitter({
   consumer_secret: nconf.get("twitter").consumerSecret,
   access_token_key: nconf.get("twitter").accessToken,
   access_token_secret: nconf.get("twitter").accessSecret
-});
+})
 
 // Rebuild Riker tweets
 function rebuildCache(maxId, tweets, tries, sinceId) {
@@ -81,15 +81,15 @@ function rebuildCache(maxId, tweets, tries, sinceId) {
   }
 
   client.get('statuses/user_timeline.json', queryObj, function(err, params, response) {
-    log.info({ sinceId: sinceId, maxId: maxId }, "Rebuilding cache");
+    log.info({ sinceId: sinceId, maxId: maxId }, "Rebuilding cache")
     if(err) {
       if(tries >= 3) {
         // Stop
-        log.error({ sinceId: sinceId, maxId: maxId, tweets: tweets, err: err }, "Failed rebuilding cache after 3 tries");
-        return;
+        log.error({ sinceId: sinceId, maxId: maxId, tweets: tweets, err: err }, "Failed rebuilding cache after 3 tries")
+        return
       }
-      log.warn({ sinceId: sinceId, maxId: maxId, err: err }, "Error querying Twitter");
-      return rebuildCache(maxId, tweets, tries+1, sinceId);
+      log.warn({ sinceId: sinceId, maxId: maxId, err: err }, "Error querying Twitter")
+      return rebuildCache(maxId, tweets, tries+1, sinceId)
     }
 
     if(!params.length) {
@@ -99,11 +99,11 @@ function rebuildCache(maxId, tweets, tries, sinceId) {
       for(var i=0, length=params.length; i<length; i++) {
         if(sinceId == params[i].id_str) {
           // We're done
-          return fillCache(tweets);
+          return fillCache(tweets)
         }
 
         if(!params[i].text || !params[i].text.length) {
-          continue;
+          continue
         }
 
         var tweet = {
@@ -114,77 +114,75 @@ function rebuildCache(maxId, tweets, tries, sinceId) {
         }
 
         if(tweet.text.length >= 1) {
-          tweet.char0 = tweet.text.substring(0,1);
+          tweet.char0 = tweet.text.substring(0,1)
         }
         if(tweet.text.length >= 2) {
-          tweet.char1 = tweet.text.substring(1,2);
+          tweet.char1 = tweet.text.substring(1,2)
         }
         if(tweet.text.length >= 3) {
-          tweet.char2 = tweet.text.substring(2,3);
+          tweet.char2 = tweet.text.substring(2,3)
         }
 
         tweets[params[i].id_str] = tweet;
 
         if(!bigIntMaxId || bigIntMaxId.compare(params[i].id_str) > 0) {
-          maxId = params[i].id_str;
+          maxId = params[i].id_str
         }
       }
     }
 
     if(maxId && maxId == lastMaxId) {
       // We're done
-      return fillCache(tweets);
+      return fillCache(tweets)
     } else {
       // Fetch more
-      return rebuildCache(maxId, tweets, 0, sinceId);
+      return rebuildCache(maxId, tweets, 0, sinceId)
     }
   });
 };
 
 function fillCache(tweets) {
   log.info("Filling cache");
-  var dbRw = new sqlite.Database(dbFile);
-  dbRw.run("DELETE FROM tweets", function(err) {
-    if(err) {
-      log.error({ err: err }, "Unable to clear cache.");
-      return;
-    }
+  const dbRw = new Database(dbFile)
+  try {
+    dbRw.prepare("DELETE FROM tweets").run()
+  } catch(err) {
+    log.error({ err: err }, "Unable to clear cache")
+    return
+  }
+  
+  dbRw.prepare("INSERT INTO tweets (t_id, tweet, char0, char1, char2) VALUES($id, $tweet, $char0, $char1, $char2);")
 
-    var stmt = dbRw.prepare("INSERT INTO tweets (t_id, tweet, char0, char1, char2) VALUES($id, $tweet, $char0, $char1, $char2);")
-    dbRw.serialize(function() {
-      for(var i in tweets) {
-        stmt.run({
-          $id: i,
-          $tweet: tweets[i].text,
-          $char0: tweets[i].char0,
-          $char1: tweets[i].char1,
-          $char2: tweets[i].char2
-        });
-      }
-    });
-
-    stmt.finalize();
-    dbRw.close();
-  });
+  var stmt = dbRw.prepare("INSERT INTO tweets (t_id, tweet, char0, char1, char2) VALUES($id, $tweet, $char0, $char1, $char2);")
+  for(let i in tweets) {
+    stmt.run({
+      id: i,
+      tweet: tweets[i].text,
+      char0: tweets[i].char0,
+      char1: tweets[i].char1,
+      char2: tweets[i].char2
+    })
+  }
+  dbRw.close();
 }
+
 rebuildCache();
 setInterval(rebuildCache, 86400000)
 
 // Routes
 server.get('/tweets.json', function(req, res) {
   res.header('Content-Type', 'application/javascript');
-  var tweets = [];
+  let tweets = [], rows = []
 
-  var query = "SELECT tweet FROM tweets";
-  db.all(query, function(err, rows) {
-    if(err) {
-      log.warn({ err: err }, "Unable to query cache.");
-      return res.status(500).jsonp(err);
-    }
+  try{
+    rows = db.prepare("SELECT tweet FROM tweets").all()
+  } catch(err) {
+    log.warn({ err: err }, "Unable to query cache.");
+    return res.status(500).jsonp(err);
+  }
 
-    tweets = rows
-    return res.jsonp(tweets);
-  });
+  tweets = rows
+  return res.jsonp(tweets)
 });
 
 var oneDay = 24*60*60*1000;
